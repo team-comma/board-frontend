ffad05a52a6e9dd7c0daa257108f30c8f4a8e530
[1O16] <youiiyou1253@gmail.com>
2022-12-16 01:33:15 +0900

    feat: index 페이지 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-  return <div>Welcome to Next.js with Yarn Berry!dasds</div>;
+  return <></>;
 };
 
 export default HomePage;
