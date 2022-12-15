691050904cb7402318c9d32ad667330e14394867
[1O16] <youiiyou1253@gmail.com>
2022-12-16 01:18:17 +0900

    feat(login): Login Container 추가

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,28 +1,45 @@
 import React from 'next';
-import type { NextPage } from 'next';
-import { AuthLayout } from 'src/components/layout';
+import { AuthLayout } from 'src/components/layouts';
+import { Input, Button } from 'src/components';
+import {
+  AuthDescription,
+  AuthForm,
+  AuthHeader,
+  AuthTitle,
+  AuthBottom,
+  AuthLink,
+} from 'src/components/layouts/AuthLayout/styled';
 import * as S from './styled';
 
-const LoginPage: NextPage = () => {
+export const LoginContainer = () => {
   return (
     <AuthLayout>
-      <S.HeaderDiv display="flex" flexDirection="column">
-        <S.LoginTitle title="x6" fontWeight="bold">
-          대시판 로그인
-        </S.LoginTitle>
-        <S.LoginDescription description="x4" margin="x0">
+      <AuthHeader>
+        <AuthTitle>대시판 로그인</AuthTitle>
+        <AuthDescription>
           한세사이버보안고등학교 • 게시판 프로젝트
-        </S.LoginDescription>
-      </S.HeaderDiv>
-      <S.LoginForm paddingTop="x6" display="flex" flexDirection="column">
-        <label htmlFor="email">아이디</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" />
-        <button type="submit">로그인</button>
-      </S.LoginForm>
+        </AuthDescription>
+      </AuthHeader>
+      <AuthForm>
+        <Input label="아이디" />
+        <Input label="비밀번호" />
+        <AuthBottom display="flex" justifyContent="spaceBetween">
+          <AuthLink>회원가입/계정찾기</AuthLink>
+          <Button
+            width="x16"
+            height="x11"
+            bgColor="mint"
+            color="white"
+            fontWeight="bold"
+            border="none"
+            borderRadius="x2"
+          >
+            로그인
+          </Button>
+        </AuthBottom>
+      </AuthForm>
     </AuthLayout>
   );
 };
 
-export default LoginPage;
+export default LoginContainer;
