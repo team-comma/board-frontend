21b80c3346358aa0958a88796b60984a52e25d04
[1O16] <youiiyou1253@gmail.com>
2022-12-16 01:15:15 +0900

    feat(auth): login and register 페이지 설정

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,8 @@
+import React, { NextPage } from 'next';
+import { LoginContainer } from 'src/containers';
+
+const LoginPage: NextPage = () => {
+  return <LoginContainer />;
+};
+
+export default LoginPage;
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,8 @@
+import React, { NextPage } from 'next';
+import { RegisterContainer } from 'src/containers/Register';
+
+const RegisterPage: NextPage = () => {
+  return <RegisterContainer />;
+};
+
+export default RegisterPage;
