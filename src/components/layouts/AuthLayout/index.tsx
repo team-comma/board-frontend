e889303186fbf6324eb06b1debab653965dd30ca
[1O16] <youiiyou1253@gmail.com>
2022-12-16 01:26:49 +0900

    feat: AuthLayout 컴포넌트 추가

--- src/components/layouts/AuthLayout/index.tsx
+++ src/components/layouts/AuthLayout/index.tsx
@@ -6,6 +6,16 @@ interface AuthLayout extends Stitches.VariantProps<typeof S.AuthLayout> {
   children?: React.ReactNode;
 }
 
-export const AuthLayout = ({ children }: AuthLayout) => {
-  return <S.AuthLayout>{children}</S.AuthLayout>;
+export const AuthLayout = ({ children, ...props }: AuthLayout) => {
+  return (
+    <>
+      <S.AuthLayout>{children}</S.AuthLayout>
+      <S.AuthHeader {...props} />
+      <S.AuthTitle {...props} />
+      <S.AuthDescription {...props} />
+      <S.AuthForm {...props} />
+      <S.AuthBottom {...props} />
+      <S.AuthLink {...props} />
+    </>
+  );
 };
--- src/components/layouts/AuthLayout/styled.ts
+++ src/components/layouts/AuthLayout/styled.ts
@@ -1,7 +1,62 @@
 import { styled } from 'stitches.config';
+import { sizes } from 'src/styles';
+
 export const AuthLayout = styled('div', {
   width: '100%',
   height: '100vh',
   margin: 'auto',
-  padding: '100px 640px 250px',
+  padding: '80px 720px',
+});
+
+export const AuthHeader = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export const AuthTitle = styled('p', {
+  fontSize: sizes.fontSizes.x6,
+  fontWeight: 'bold',
+  marginBottom: sizes.space.x2,
+});
+
+export const AuthDescription = styled('p', {
+  fontSize: sizes.fontSizes.x4,
+  margin: sizes.space.x0,
+});
+
+export const AuthForm = styled('form', {
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: sizes.space.x2,
+});
+
+export const AuthBottom = styled('div', {
+  marginTop: sizes.space.x9,
+  variants: {
+    display: {
+      flex: { display: 'flex' },
+    },
+    flexDirection: {
+      row: { flexDirection: 'row' },
+    },
+    justifyContent: {
+      spaceBetween: { justifyContent: 'space-between' },
+    },
+    alignItem: {
+      center: { alignContent: 'center' },
+    },
+  },
+});
+
+export const AuthLink = styled('a', {
+  fontSize: sizes.fontSizes.x3,
+  fontWeight: 'bold',
+  variants: {
+    marginTop: {
+      x3: { marginTop: sizes.fontSizes.x3 },
+    },
+    marginLeft: {
+      x1: { marginLeft: sizes.space.x1 },
+    },
+  },
 });
