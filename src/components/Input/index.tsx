8057e32d3158e711382bc8a9e90428da5c19c6ca
[1O16] <youiiyou1253@gmail.com>
2022-12-16 01:30:05 +0900

    feat: Input 컴포넌트에 label 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,16 @@
 import React from 'next';
+import type * as Stitches from '@stitches/react';
 import * as S from './styled';
 
-export const Input = ({ ...props }) => {
-  return <S.InputElements {...props} />;
+interface InputProps extends Stitches.VariantProps<typeof S.InputElements> {
+  label?: string;
+}
+
+export const Input = ({ label, ...props }: InputProps) => {
+  return (
+    <>
+      <S.InputLabel>{label && <div>{label}</div>}</S.InputLabel>
+      <S.InputElements {...props} />
+    </>
+  );
 };
--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -1,15 +1,22 @@
 import { colors, sizes } from 'src/styles';
 import { styled } from 'stitches.config';
 
+export const InputLabel = styled('label', {
+  fontSize: sizes.fontSizes.x4,
+  marginTop: sizes.space.x8,
+  paddingBottom: sizes.space.x2,
+});
+
 export const InputElements = styled('input', {
   variants: {
     width: {
       x80: { width: sizes.space.x80 },
     },
-    height: {
-      x12: { height: sizes.space.x12 },
+    marginTop: {
+      x2: { marginTop: sizes.space.x2 },
     },
   },
+  height: sizes.space.x11,
   padding: sizes.space.x2,
   borderRadius: sizes.space.x2,
   outline: 'none',
