29edbf5ecdd4e9338890a45a48e310544771a82b
[1O16] <youiiyou1253@gmail.com>
2022-12-16 20:28:49 +0900

    fix: AuthLayout 컴포넌트 Object.assign 으로 스타일 export

--- src/components/layouts/AuthLayout/index.tsx
+++ src/components/layouts/AuthLayout/index.tsx
@@ -6,16 +6,19 @@ interface AuthLayout extends Stitches.VariantProps<typeof S.AuthLayout> {
   children?: React.ReactNode;
 }
 
-export const AuthLayout = ({ children, ...props }: AuthLayout) => {
+export const AuthLayout = ({ children }: AuthLayout) => {
   return (
     <>
       <S.AuthLayout>{children}</S.AuthLayout>
-      <S.AuthHeader {...props} />
-      <S.AuthTitle {...props} />
-      <S.AuthDescription {...props} />
-      <S.AuthForm {...props} />
-      <S.AuthBottom {...props} />
-      <S.AuthLink {...props} />
     </>
   );
 };
+
+export const Auth = Object.assign(AuthLayout, {
+  Header: S.AuthHeader,
+  Title: S.AuthTitle,
+  Description: S.AuthDescription,
+  Form: S.AuthForm,
+  Bottom: S.AuthBottom,
+  Link: S.AuthLink,
+});
