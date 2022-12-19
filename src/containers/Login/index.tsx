f261ef4d91700c6960612b6b7a4ad31ce7918ee8
gull2365 <jych2365@gmail.com>
2022-12-19 22:17:28 +0900

    feat: 로그인 로직 추가

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,30 +1,60 @@
 import React from 'next';
-import { AuthLayout } from 'src/components/layouts';
+import { Auth, AuthLayout } from 'src/components/layouts';
 import { Input, Button } from 'src/components';
-import {
-  AuthDescription,
-  AuthForm,
-  AuthHeader,
-  AuthTitle,
-  AuthBottom,
-  AuthLink,
-} from 'src/components/layouts/AuthLayout/styled';
+import { useForm } from 'react-hook-form';
 import * as S from './styled';
 
+type HookFormTypes = {
+  username: string;
+  password: string;
+};
+
 export const LoginContainer = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<HookFormTypes>();
+
+  const onSubmit = (props: HookFormTypes) => {
+    console.log(props);
+  };
+
   return (
     <AuthLayout>
-      <AuthHeader>
-        <AuthTitle>대시판 로그인</AuthTitle>
-        <AuthDescription>
+      <Auth.Header>
+        <Auth.Title>대시판 로그인</Auth.Title>
+        <Auth.Description>
           한세사이버보안고등학교 • 게시판 프로젝트
-        </AuthDescription>
-      </AuthHeader>
-      <AuthForm>
-        <Input label="아이디" />
-        <Input label="비밀번호" />
-        <AuthBottom display="flex" justifyContent="spaceBetween">
-          <AuthLink>회원가입/계정찾기</AuthLink>
+        </Auth.Description>
+      </Auth.Header>
+      <Auth.Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="text"
+          label="아이디"
+          {...register('username', {
+            required: '올바른 아이디 또는 비밀번호를 입력해주세요.',
+            minLength: {
+              value: 8,
+              message: '아이디는 8글자 이상이여야 합니다.',
+            },
+          })}
+        />
+        <Auth.ErrorMessage>{errors.username?.message}</Auth.ErrorMessage>
+        <Input
+          type="password"
+          label="비밀번호"
+          {...register('password', {
+            required: '올바른 아이디 또는 비밀번호를 입력해주세요.',
+            minLength: {
+              value: 8,
+              message: '비밀번호는 8자 이상이여야 합니다.',
+            },
+          })}
+        />
+        <Auth.ErrorMessage>{errors.password?.message}</Auth.ErrorMessage>
+        <Auth.Bottom display="flex" justifyContent="spaceBetween">
+          <Auth.Link href="/auth/register">회원가입/계정찾기</Auth.Link>
           <Button
             width="x16"
             height="x11"
@@ -36,8 +66,8 @@ export const LoginContainer = () => {
           >
             로그인
           </Button>
-        </AuthBottom>
-      </AuthForm>
+        </Auth.Bottom>
+      </Auth.Form>
     </AuthLayout>
   );
 };
